70d8c28cb490ab01b71c97442e854eb0677c1b6a
Michael van de Water <me@mjvdw.com>
2023-11-18 18:12:14 +1300

    Loads JSON file into string.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate serde;
+extern crate serde_json;
+
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    load_risk_framework();
+    // println!("Hello, world!");
+}
+
+fn load_risk_framework() {
+    let mut file = File::open("src/framework.json").expect("Unable to open file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Unable to read file");
+    println!("{}", contents);
 }
